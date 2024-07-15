782a7a8f5e3fff6f832b72f00cfb9e42054a2644
Brian Wang <bubwb12@gmail.com>
2024-07-15 17:50:25 -0400

    linked functionality to buttons

--- addFruit.js
+++ addFruit.js
@@ -47,6 +47,8 @@ const fruits = popFruits(initFruitTypes);
 
 let currentPhase = 0;
 
+// display fruits in the initial container
+
 function setFruits(imagePaths) {
     // clear container if there are still leftover fruits from last phase
     initialContainer.innerHTML = '';
@@ -61,3 +63,90 @@ function setFruits(imagePaths) {
 }
 
 setFruits(fruits[currentPhase]);
+
+// movement logic 
+// function to move fruits from initial container to end container
+
+function moveFruits() {
+    if (initialContainer.children.length > 0) {
+        // select the last fruit in the initial container
+        const fruit = initialContainer.children[initialContainer.children.length - 1];
+        endContainer.appendChild(fruit);
+    }
+}
+
+// use spacebar to trigger moveFruits()
+
+document.addEventListener('keypress', function(event) {
+    if (event.code === 'Space') {
+        event.preventDefault();
+
+        moveFruits();
+    }
+});
+
+// use button to trigger moveFruits()
+
+const moveFruitButton = document.getElementById('add-fruit');
+
+moveFruitButton.addEventListener('click', function() {
+    moveFruits();
+});
+
+// remove fruit from end container and put back into initial container
+
+function removeFruits() {
+    if (endContainer.children.length > 0) {
+        const fruit = endContainer.children[endContainer.children.length - 1];
+        initialContainer.appendChild(fruit);
+    }
+
+}
+
+// use button to trigger removeFruits()
+
+const removeFruitButton = document.getElementById('remove-fruit');
+
+removeFruitButton.addEventListener('click', function() {
+    removeFruits();
+});
+
+// clear fruit from end container and restore all fruits back to initial container
+
+function clearFruits() {
+    while (endContainer.children.length > 0) {
+        const fruit = endContainer.children[0];
+        initialContainer.appendChild(fruit);
+    }
+}
+
+// use R key to trigger clearFruits()
+
+document.addEventListener('keypress', function(event) {
+    if (event.code === 'KeyR') {
+        event.preventDefault();
+
+        clearFruits();
+    }
+});
+
+// use button to trigger clearFruits()
+
+const clearFruitButton = document.getElementById('clear-fruit');
+
+clearFruitButton.addEventListener('click', function() {
+    clearFruits();
+});
+
+// prevent spacebar from triggering the buttons
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('keypress', event => {
+        if (event.key === " " || event.key === "Spacebar") {
+            event.preventDefault();
+            event.target.blur();
+        }
+    })
+});
